cc454592da278e68f8621a35b088a59b8a79bf1b
iamzwq <876867644@qq.com>
2024-11-25 22:35:10 +0800

    feat: 🌅 添加主题切换动画效果

--- src/components/theme-switch/index.tsx
+++ src/components/theme-switch/index.tsx
@@ -3,7 +3,35 @@ import { Dropdown, type MenuProps } from "antd";
 import { useTheme } from "../theme-provider";
 
 export function ThemeSwitch() {
-  const { theme, setTheme } = useTheme();
+  const { theme, setTheme, isDarkMode } = useTheme();
+
+  const handleChange = async (theme: "system" | "light" | "dark") => {
+    const handler = () => {
+      setTheme(theme);
+    };
+    if (!document.startViewTransition) {
+      handler();
+      return;
+    }
+
+    await document.startViewTransition(handler).ready;
+
+    const r = Math.hypot(window.innerWidth, window.innerHeight);
+    document.documentElement.animate(
+      {
+        clipPath: [
+          `circle(0px at ${window.innerWidth}px 0px)`,
+          `circle(${r}px at ${window.innerWidth}px 0px)`,
+        ],
+      },
+      {
+        duration: 500,
+        easing: "ease-in",
+        direction: isDarkMode ? "normal" : "reverse",
+        pseudoElement: isDarkMode ? `::view-transition-new(root)` : `::view-transition-old(root)`,
+      },
+    );
+  };
 
   const items: MenuProps["items"] = [
     {
@@ -13,7 +41,7 @@ export function ThemeSwitch() {
         </div>
       ),
       key: "0",
-      onClick: () => setTheme("light"),
+      onClick: () => handleChange("light"),
     },
     {
       label: (
@@ -22,7 +50,7 @@ export function ThemeSwitch() {
         </div>
       ),
       key: "1",
-      onClick: () => setTheme("dark"),
+      onClick: () => handleChange("dark"),
     },
     {
       label: (
@@ -31,7 +59,7 @@ export function ThemeSwitch() {
         </div>
       ),
       key: "3",
-      onClick: () => setTheme("system"),
+      onClick: () => handleChange("system"),
     },
   ];
 
